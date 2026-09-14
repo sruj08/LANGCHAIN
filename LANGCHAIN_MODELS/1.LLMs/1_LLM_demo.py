0582824eec22b10e9d233d8b3ca94c8369a49e7b
sruj08 <satavsrujan@gmail.com>
2026-09-14 19:11:43 +0530

    mistakely implemented chatmodel instead of LLM

--- LANGCHAIN_MODELS/1.LLMs/1_LLM_demo.py
+++ LANGCHAIN_MODELS/1.LLMs/1_LLM_demo.py
@@ -1,14 +1,14 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = GoogleGenerativeAI(model="gemini-2.5-flash")
 
 
 response = llm.invoke("What is the capital of India")
 
-print(response.content)
+print(response)
 
 # What is the capital of India
 
